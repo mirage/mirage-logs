0c14970494589bff42c22ceef1073427011be98b
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2019-03-28 14:50:49 +0100

    Switch to dune.

--- pkg/pkg.ml
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ocaml
-#use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "mirage-logs" @@ fun c ->
-  Ok [ Pkg.mllib "src/mirage-logs.mllib";
-       Pkg.test "test/test"; ]
